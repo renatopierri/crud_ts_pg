e0ad7bb54db82b90b9502b633ed20480723c2e2c
Renato de Pierri <renatopierri@users.noreply.github.com>
2024-08-15 15:15:55 -0300

    Criação da atividade 04

--- src/routes.ts
+++ src/routes.ts
@@ -1,31 +1,29 @@
 import { Router } from 'express';
 import { Pool } from 'pg';
-
 const router = Router();
-
 const pool = new Pool({
   host: 'localhost',
   user: 'postgres',
-  password: 'sua_senha',
-  database: 'ts_crud',
+  password: '123456',
+  database: 'db_ts_crud',
   port: 5432,  // porta padrão do PostgreSQL
 });
-
 // Rota de índice
 router.get('/', (req, res) => {
   res.send('Welcome to the API');
 });
 
+
 // Rota GET para inserir dados no banco de dados
-router.get('/insere', async (req, res) => {
-  const { name, description } = req.query;
-  
+router.get('/get_insere', async (req, res) => {
+  const {name, description } = req.query;
   if (!name || !description) {
     return res.status(400).send('Name and description are required');
   }
 
+
   try {
-    const result = await pool.query('INSERT INTO db_items (name, description) VALUES ($1, $2) RETURNING *', [name, description]);
+    const result = await pool.query('INSERT INTO db_ts_crud.public.tabela_ts_crud (name, description) VALUES ($1, $2) RETURNING *', [name, description]);
     res.status(201).send(result.rows[0]);
   } catch (err) {
     res.status(500).send(err);
@@ -34,12 +32,27 @@ router.get('/insere', async (req, res) => {
 
 //Falta montar:
 //Rota GET para listar dados do banco de dados
+//router.get('/get_select', ...
+
 //Rota GET para atualizar dados no banco de dados
+//router.get('/get_update', ...
+
 //Rota GET para excluir dados do banco de dados
+//router.get('/get_delete', ...
+
+//====================================================
 
 //Rota POST para inserir dados do banco de dados
+//router.post('/post_inserir', ...
+
 //Rota POST para listar dados do banco de dados
+//router.post('/post_listar', ...
+
 //Rota POST para atualizar dados no banco de dados
+//router.post('/post_atualizar', ...
+
 //Rota POST para excluir dados do banco de dados
+//router.post('/post_excluir/:id', ...
+
 
 export default router;
